6c021d53fb2fa3b956eac0197db8629cccafb2a1
Gildeir <gildeirlopes@gmail.com>
2026-01-19 21:53:51 -0300

    feature(mappings): adding habitMappings, HabitQueries and Dtos

--- DevHabit/DevHabit.Api/DTOs/Habits/HabitMappings.cs
+++ DevHabit/DevHabit.Api/DTOs/Habits/HabitMappings.cs
@@ -1,6 +1,72 @@
-﻿namespace DevHabit.Api.DTOs.Habits;
+﻿using System.Linq.Expressions;
+using DevHabit.Api.Entities;
 
-public class HabitMappings
-{
-    
+namespace DevHabit.Api.DTOs.Habits;
+
+internal static class HabitMappings
+{ 
+    public static HabitDto ToDto(this Habit habit)
+    {
+        return new HabitDto
+        {
+            Id = habit.Id,
+            Name = habit.Name,
+            Description = habit.Description,
+            Type = habit.Type,
+            Frequency = new FrequencyDto
+            {
+                Type = habit.Frequency.Type,
+                TimesPerPeriod = habit.Frequency.TimesPerPeriod
+            },
+            Target = new TargetDto
+            {
+                Value = habit.Target.Value,
+                Unit = habit.Target.Unit
+            },
+            Status = habit.Status,
+            IsArchived = habit.IsArchived,
+            EndDate = habit.EndDate,
+            Milestone = new MilestoneDto
+            {
+                Current = habit.Milestone!.Current, 
+                Target = habit.Milestone.Target
+            },
+            CreatedAtUtc = habit.CreatedAtUtc,
+            UpdatedAtUtc = habit.UpdatedAtUtc,
+            LastCompletedAtUtc = habit.LastCompletedAtUtc
+        };
+    }
+    public static Habit ToEntity(this CreateHabitDto dto)
+    {
+        Habit habit = new()
+        {
+            Id = $"h_{Guid.CreateVersion7()}",
+            Name = dto.Name,
+            Description = dto.Description,
+            Type = dto.Type,
+            Frequency = new Frequency
+            {
+                Type = dto.Frequency.Type,
+                TimesPerPeriod = dto.Frequency.TimesPerPeriod
+            },
+            Target = new Target
+            {
+                Value = dto.Target.Value,
+                Unit = dto.Target.Unit
+            },
+            Status = HabitStatus.Ongoing,
+            IsArchived = false,
+            EndDate = dto.EndDate,
+            Milestone = dto.Milestone is not null
+                ? new Milestone
+                {
+                    Target = dto.Milestone.Target,
+                    Current = 0 // Initialize current progress to 0
+                }
+                : null,
+            CreatedAtUtc = DateTime.UtcNow
+        };
+        
+        return habit;
+    }
 }
