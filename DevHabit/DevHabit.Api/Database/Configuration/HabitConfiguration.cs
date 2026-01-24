7faf5cf35893df481b4d3fc2c62967937ea10d91
Gildeir <gildeirlopes@gmail.com>
2026-01-23 23:51:48 -0300

    feat(HabitTags) setup HatitTags database

--- DevHabit/DevHabit.Api/Database/Configuration/HabitConfiguration.cs
+++ DevHabit/DevHabit.Api/Database/Configuration/HabitConfiguration.cs
@@ -23,6 +23,10 @@ public sealed class HabitConfiguration: IEntityTypeConfiguration<Habit>
         });
 
         builder.OwnsOne(h => h.Milestone);
+
+        builder.HasMany(h => h.Tags)
+            .WithMany()
+            .UsingEntity<HabitTag>();
     }
     
 }
