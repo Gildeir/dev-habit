1849e89f1f0131e6b91c2626fc3b353c23486e2d
Gildeir <gildeirlopes@gmail.com>
2026-02-09 23:05:58 -0300

    feat(tagValidator) add tag validator file

--- DevHabit/DevHabit.Api/DTOs/Tags/CreateTagDto.cs
+++ DevHabit/DevHabit.Api/DTOs/Tags/CreateTagDto.cs
@@ -8,12 +8,3 @@ public sealed record CreateTagDto
     public string? Description { get; set; }
 }
 
-public sealed class CreateTagDtoValidator : AbstractValidator<CreateTagDto>
-{
-    public CreateTagDtoValidator()
-    {
-        RuleFor(x => x.Name).NotEmpty().MinimumLength(3);
-        RuleFor(x => x.Description).MaximumLength(50);
-    }
-}
-
